4a38fbfc292f036045ec9e6a47cca6aeceb65fa6
CSant04y <esquivelcarlos27893@gmail.com>
2022-02-01 14:31:04 -0600

    added task

---
+++ 0x00-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,17 @@
+namespace Subjects {
+    export interface TeacherInterface {
+        experienceTeachingReact?: number;
+    }
+    export class Java extends Subject {
+        getRequirements(): string {
+            return "Here is the list of requirements for Java";
+        }
+        getAvailableTeacher(): string {
+            if (!this.teacher.experienceTeachingJava) {
+                return "No available teacher";
+            } else {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            }
+        }
+    }
+}
