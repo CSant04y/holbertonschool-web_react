85c60847f57bffdde3b2e538180552592dfb4bf6
CSant04y <esquivelcarlos27893@gmail.com>
2022-02-03 12:26:10 -0600

    added index file

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -2,6 +2,5 @@ import $ from "jquery";
 let paragraphs = ["Holberton Dashboard", "Dashboard data for the students", "Copyright - Holberton School"];
 
 for (let i = 0; i < paragraphs.length; i++) {
-    let p = $("p").add(paragraphs[i])
-    p.appendTo.$(document.body);
+    $("p").add(paragraphs[i]).appendTo(document.body);
 }
