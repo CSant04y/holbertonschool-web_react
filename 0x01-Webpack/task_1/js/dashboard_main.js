38aad92aa327b6954c98be0917cc5192740ed8d6
CSant04y <esquivelcarlos27893@gmail.com>
2022-02-03 16:07:23 -0600

    added task

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,17 @@
-const $ = require('jquery');
-const _ = require('lodash');
+import $ from 'jquery';
+import { debounce } from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
 
 let count = 0;
 function updateCounter() {
     count = count + 1;
-    $('#count').text(`${count} clicks on the button`);
+    $('#count').html(`${count} clicks on the button`);
 }
 
-$('button').on('click', _.debounce(updateCounter, 500));
+$('button').on('click', debounce(updateCounter, 500));
