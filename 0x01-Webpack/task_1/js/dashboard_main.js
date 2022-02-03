f8c443b6558291ef35d22eed271b3654f0f6d324
CSant04y <esquivelcarlos27893@gmail.com>
2022-02-03 16:04:34 -0600

    added task

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -7,10 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('button').on('click', _.debounce(updateCounter, 500));
 
 let count = 0;
 function updateCounter() {
-  count = count + 1;
-  $('#count').text(`${count} clicks on the button`);
+    count = count + 1;
+    $('#count').text(`${count} clicks on the button`);
 }
+
+$('button').on('click', _.debounce(updateCounter, 500));
