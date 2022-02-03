0a6fe5c759bb1a1e249819cc68ef377557b5a1f9
CSant04y <esquivelcarlos27893@gmail.com>
2022-02-03 17:48:26 -0600

    added congif

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -9,14 +9,46 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    use: [
+                        {
+                          loader: 'img-optimize-loader',
+                          options: {
+                            compress: {
+                              // loseless compression for png
+                              optipng: {
+                                optimizationLevel: 4,
+                              },
+                              // lossy compression for png. This will generate smaller file than optipng.
+                              pngquant: {
+                                quality: [0.2, 0.8],
+                              },
+                              // Compression for webp.
+                              // You can also tranform jpg/png into webp.
+                              webp: {
+                                quality: 100,
+                              },
+                              // Compression for svg.
+                              svgo: true,
+                              // Compression for gif.
+                              gifsicle: {
+                                optimizationLevel: 3,
+                              },
+                              // Compression for jpg.
+                              mozjpeg: {
+                                progressive: true,
+                                quality: 60,
+                              },
+                            },
+                          },
+                        },
+                      ],
+                },
         ],
     },
 };
