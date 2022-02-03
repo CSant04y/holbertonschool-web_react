1ec1764915680e2f33487f9c92116f84b22a53b0
CSant04y <esquivelcarlos27893@gmail.com>
2022-02-03 16:14:11 -0600

    added func

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -7,9 +7,8 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-
 let count = 0;
-function updateCounter() {
+const updateCounter = () => {
     count = count + 1;
     $('#count').html(`${count} clicks on the button`);
 }
