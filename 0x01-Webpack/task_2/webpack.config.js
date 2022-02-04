6baed9618663d80898d7beb7907a2e425537681a
CSant04y <esquivelcarlos27893@gmail.com>
2022-02-03 18:17:07 -0600

    added config

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,54 +1,47 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './js/dashboard_main.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-                {
-                    test: /\.css$/i,
-                    use: ["style-loader", "css-loader"],
-                },
-                {
-                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                    use: [
-                        {
-                          loader: 'img-optimize-loader',
-                          options: {
-                            compress: {
-                              // loseless compression for png
-                              optipng: {
-                                optimizationLevel: 4,
-                              },
-                              // lossy compression for png. This will generate smaller file than optipng.
-                              pngquant: {
-                                quality: [0.2, 0.8],
-                              },
-                              // Compression for webp.
-                              // You can also tranform jpg/png into webp.
-                              webp: {
-                                quality: 100,
-                              },
-                              // Compression for svg.
-                              svgo: true,
-                              // Compression for gif.
-                              gifsicle: {
-                                optimizationLevel: 3,
-                              },
-                              // Compression for jpg.
-                              mozjpeg: {
-                                progressive: true,
-                                quality: 60,
-                              },
-                            },
-                          },
-                        },
-                      ],
-                },
-        ],
-    },
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [ "style-loader", "css-loader" ],
+      },
+      {
+	test: /\.(gif|png|jpe?g|svg)$/i,
+	use: [
+	  'file-loader',
+	  {
+	    loader: 'image-webpack-loader',
+	    options: {
+              mozjpeg: {
+		progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+		enabled: false,
+              },
+              pngquant: {
+		quality: [0.65, 0.90],
+		speed: 4
+              },
+              gifsicle: {
+		interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+		quality: 75
+              }
+	    }
+	  },
+	],
+      }
+    ],
+  },
 };
