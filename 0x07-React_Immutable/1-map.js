ba2691490ffe7cb6f6c975546cb441f54f7df84e
CSant04y <esquivelcarlos27893@gmail.com>
2022-03-07 09:45:31 -0600

    Used Map to create obj

---
+++ 0x07-React_Immutable/1-map.js
@@ -0,0 +1,16 @@
+import { Map } from 'immutable';
+
+// Take in Object and returns Immutable object
+export default function getImmutableObject(object) {
+    return Map(object);
+};
+
+const object = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+};
+const obj = getImmutableObject(object);
+
+console.log(obj);
