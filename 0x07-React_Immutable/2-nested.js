da9749534752e61c5e4997c0c51e790eff7af3fe
CSant04y <esquivelcarlos27893@gmail.com>
2022-03-08 13:29:08 -0600

    fixed code formatting

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -2,7 +2,6 @@ import { fromJS, Map } from "immutable";
 
 //Coverts object into Map and finds nested value with getIn
 export default function accessImmutableObject(object, array) {
-    const mapObj = fromJS(object);
-
-    return mapObj.getIn(array, undefined);
+  const mapObj = fromJS(object);
+  return mapObj.getIn(array, undefined);
 }
