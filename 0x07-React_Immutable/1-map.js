c73125d31682297282a6085a2a2bbdea713be18b
CSant04y <esquivelcarlos27893@gmail.com>
2022-03-08 13:28:25 -0600

    fixed code fomatting

--- 0x07-React_Immutable/1-map.js
+++ 0x07-React_Immutable/1-map.js
@@ -3,13 +3,13 @@ import { Map } from 'immutable';
 // Take in Object and returns Immutable object
 export default function getImmutableObject(object) {
   return Map(object);
-};
+}
 
 const object = {
   fear: true,
   smell: -1033575916.9145899,
   wall: false,
-  thing: -914767132
+  thing: -914767132,
 };
 const obj = getImmutableObject(object);
 
