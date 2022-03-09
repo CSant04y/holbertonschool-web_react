995a7308f59291f0187956a7fa5840b880e0d377
CSant04y <esquivelcarlos27893@gmail.com>
2022-03-09 08:48:19 -0600

    added condensed return

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,8 +1,5 @@
 import { Map } from 'immutable';
 
 export function mergeDeeplyElements(page1, page2) {
-    const p1 = Map(page1);    
-    const p2 = Map(page2);
-
-    return p1.mergeDeep(p2);
+  return (Map(page1).mergeDeep(Map(page2)));
 }
