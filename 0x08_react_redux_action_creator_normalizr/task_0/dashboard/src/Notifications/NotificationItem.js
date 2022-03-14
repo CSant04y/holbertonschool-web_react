927b193a6020f6d275c6901b554020217084d422
CSant04y <esquivelcarlos27893@gmail.com>
2022-03-14 11:01:31 -0600

    added task 0

---
+++ 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite'
+
+class NotificationItem extends React.Component {
+    render() {
+        let {
+            id,
+            type,
+            value,
+            html,
+            className,
+            // markAsRead
+        } = this.props;
+
+        let styleLi = (type === "urgent") ? styles.urgentNotif : styles.defaultNotif;
+        
+        if (!html) {
+            return (
+                <li data-notification-type={type}
+                className={css(styleLi)}
+                onClick={() => this.props.markAsRead(id)}
+                >{value}</li>
+            )
+        } else {
+            return (
+                <li data-notification-type={type}
+                key={id}
+                className={css(styleLi)}
+                dangerouslySetInnerHTML={html}
+                className={className}
+                onClick={() => this.props.markAsRead(id)}
+                ></li>
+            );
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+	defaultNotif: {
+		color: 'blue',
+	},
+	urgentNotif: {
+		color: 'red',
+	},
+});
+
+NotificationItem.defaultProps = {
+    type: "default",
+    value: "",
+    html: null,
+    markAsRead: () => {},
+    id: NaN
+}
+
+NotificationItem.propTypes = {
+    type: PropTypes.string,
+    value: PropTypes.string,
+    html: PropTypes.shape({
+      __html: PropTypes.string,
+    }),
+    markAsRead: PropTypes.func,
+    id: PropTypes.number
+}
+
+export default NotificationItem;
